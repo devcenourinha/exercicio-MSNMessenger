f3342190c9b027021f34e3651d7c70c6e380c093
devcenourinha <gabrielbuenotl2@gmail.com>
2024-09-18 14:46:46 -0300

    encapsulamento

---
+++ Exercicio-MSNMessenger/src/MSNMessenger.java
@@ -0,0 +1,18 @@
+public class MSNMessenger {
+    public void enviarMensagem(){
+        validarConectadoInternet();
+        System.out.println("Enviando mensagem");
+        salvarHistoricoMensagem();
+    }
+    public void receberMensagem(){
+        System.out.println("Recebendo mensagem");
+    }
+    private void validarConectadoInternet(){
+        System.out.println("Velidando se esta conectado a internet");
+    }
+    private void salvarHistoricoMensagem(){
+        System.out.println("Salvando historico de mensagem");
+    }
+
+}
+       
